63a8f12dccd8e9aa90d9e1821ae68a7fde8ea4e8
AnnaLee1215 <ec460520@gmail.com>
2022-11-23 16:25:50 +0800

    Create DBConfig.java

---
+++ TomCat/src/main/java/hitstd/group/tool/database/DBConfig.java
@@ -0,0 +1,12 @@
+package hitstd.group.tool.database;
+
+public class DBConfig {
+	public DBConfig(){		
+	}
+	
+	public String FilePath(){
+	   //DB FilePath
+		 String DBPath="C:\\Users\\User\\Documents\\GitHub\\TomCat\\TomCat\\src\\main\\webapp\\member.accdb";
+	   return DBPath;		
+	}
+}
